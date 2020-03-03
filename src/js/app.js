0d9f94b5622406fe669469794db9c68aa7296192
hugo borini <hborini@localhost.localdomain>
2020-03-03 14:12:47 -0500

    {add} first project and start responsiv

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,38 @@ $(document).ready(function() {
   $(".main").css("display", "none");
   $(".name").css("display", "none");
   $(".name__project").css("display", "none");
+  $(".sale-temps").css("display", "none");
+  $(".header").css("display", "none");
+  $(".display").css("display", "none");
+  $(".sale-temps__descrip").css("display", "none");
   setTimeout(() => {
     $(".load").css("display", "none");
     $(".main").css("display", "block");
     $(".name").fadeIn(500);
+    $(".header").fadeIn(500);
   }, 1500);
   setTimeout(() => {
     $(".name__project").fadeIn(1000);
   }, 1600);
+
+  $(".name__project").click(function() {
+    $(".name__first").fadeOut(800);
+    $(".name__last").fadeOut(800);
+    setTimeout(() => {
+      $("body").addClass("fill-blue");
+      $(".name").css("display", "none");
+      $(".sale-temps").fadeIn(1000);
+      $(".sale-temps__title").addClass("flex-slide");
+    }, 800);
+    setTimeout(() => {
+      $(".name").css("display", "none");
+      $(".display").fadeIn(800);
+    }, 1500);
+    setTimeout(() => {
+      $(".sale-temps__descrip").fadeIn(1000);
+      $(".sale-temps__descrip").addClass("slide-left");
+    }, 1700);
+  });
 });
 
 const $background = document.querySelector(".background");
